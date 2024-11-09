24ff574eb042314d2d3021890263480e0b77ab40
Rhythmicc <rhythmlian.cn@gmail.com>
2024-11-09 23:04:18 +0800

    update

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  echo(info, "Hello World!");
-  echo(success, "Success!");
-  echo(debug, "Debug!");
-  echo(error, "Error!");
-  echo(split, "Split!");
-  return 0;
+    echo(info, "Hello World! %d", 2024);
+    echo(success, "Success!");
+    echo(debug, "Debug!");
+    echo(error, "Error!");
+    echo(title, "Title");
+    echo(rule, "Rule");
+    echo(markdown, "# Markdown Title\\n1. List 1\\n2. List 2");
+    echo(warning, "Warning!");
+    echo(start_status, "Start Status");
+    echo(stop_status, "Stop Status");
+    return 0;
 }
